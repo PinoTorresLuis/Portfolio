2577dc13c7a7647021b8be1cde24e3e9b2b1c312
Pino Luis <pinotorres.luis@gmail.com>
2022-12-06 12:22:31 -0300

    se hicieron cambios en about

--- src/Componentes/About/About.jsx
+++ src/Componentes/About/About.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import img from '../../media/My project-1.png';
+import img from '../../media/imgtest 1.png';
 
 import './about.scss';
 
 const About = () => {
   return (
     <div className='about_container'><a name="about"></a>
+     <div className='wave_container_top'>
+          <svg className='wave_svg_top' viewBox="0 0 500 150" preserveAspectRatio="none">
+          <path className='path_svg_top' d="M-0.27,74.52 C179.73,276.81 466.42,-65.61 500.84,63.66 L500.00,150.00 L0.00,150.00 Z"></path>
+          </svg>
+          </div>
       <div className='about_me'>
         <div data-aos="fade-right">
           <h1 className='title_about'>Let me tell you something about me</h1>
