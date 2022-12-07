c607eee8f914d27c126f6a96b92abcabfe984e03
Pino Luis <pinotorres.luis@gmail.com>
2022-12-07 10:33:03 -0300

    se sigue trabajando en about, falta trabajar en el responsive

--- src/Componentes/About/About.jsx
+++ src/Componentes/About/About.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import img from '../../media/imgtest 1.png';
+import img from '../../media/imgtest 1.jpeg';
 
 import './about.scss';
 
 const About = () => {
   return (
     <div className='about_container'><a name="about"></a>
-     <div className='wave_container_top'>
-          <svg className='wave_svg_top' viewBox="0 0 500 150" preserveAspectRatio="none">
-          <path className='path_svg_top' d="M-0.27,74.52 C179.73,276.81 466.42,-65.61 500.84,63.66 L500.00,150.00 L0.00,150.00 Z"></path>
-          </svg>
-          </div>
       <div className='about_me'>
         <div data-aos="fade-right">
           <h1 className='title_about'>Let me tell you something about me</h1>
-          <p className='subtitle_about'>I am a web developer with excellent comunication skills. I love collaborationg and working as a team, I am looking for a full time position in web development where I can apply my knowledge and skills to continue improving</p>
+          <p className='subtitle_about'>I am a web developer with excellent comunication skills. I love collaborationg and working as a team, I am looking for a full time position in web development where I can apply my knowledge and skills to continue improving.</p>
         </div> 
       </div>  
         <div className="about_img" data-aos="fade-left" >
